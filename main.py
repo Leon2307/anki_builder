d49f634839a61e9741775aabc480c8708470ea07
Leon Fischer <leonfischer20031@web.de>
2023-05-23 13:33:20 +0200

    Delets all unimportant files

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import argparse
 import anki
 import word
-import test_alternative
 
 if __name__ == "__main__":
 
@@ -12,7 +11,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     """Gets the headers and subsection as a dictionary from a word file"""
-    section_text = test_alternative.get_paragraphes(args.docx_path)
+    section_text = word.get_paragraphes(args.docx_path)
 
     """Convert dictionary to Anki file"""
     anki.dict_to_anki(section_text, args.deck_name)
--- word.py
+++ word.py
@@ -1,45 +1,21 @@
-import zipfile    
-import xml.etree.ElementTree as ET
+from docx.api import Document
 
-ns = {'w': 'http://schemas.openxmlformats.org/wordprocessingml/2006/main'}
+def get_paragraphes(doc):
+    document = Document(doc)
+    
+    text = []
 
-def get_text(docx_path):
-    """Loading Word document and converting to XML"""
-    file_path = docx_path
-    doc = zipfile.ZipFile(file_path).read('word/document.xml')
-    root = ET.fromstring(doc)
-    f= open("document.xml", "w")
-    f.write(str(doc))
+    for paragraph in document.paragraphs:
+        if paragraph.style.name == 'Heading 2':
+            text.append({"title":paragraph.text, "text":""})
+            continue
+        elif paragraph.style.name.startswith("Heading"):
+            continue
 
-    """Get text sections from XML file"""
-    body = root.find('w:body', ns)
-    p_sections = body.findall('w:p', ns)
+        if(len(text) > 0):
+            text[len(text)-1]["text"] += "\n" + paragraph.text
+        
+    return text
 
-    """Get text in each section"""
-    text_elems = []
-    for p in p_sections:
-        text_elems.append(p.findall('.//w:t', ns))
-        print([t.text for t in p])
-        print()
-
-    """Differentiating between header and subsection"""
-    section_labels = [get_section_text(s,text_elems) if is_heading2_section(s) else '' for s in p_sections]
-    section_text = [{"title": t, "text": get_section_text(p_sections[i+1],text_elems)} for i, t in enumerate(section_labels) if len(t) > 0]
-    return section_text
-
-def is_heading2_section(p):
-    """Returns True if the given paragraph section has been styled as a Heading2"""
-    return_val = False
-    heading_style_elem = p.find(".//w:pStyle[@w:val='Heading2']", ns)
-    if heading_style_elem is not None:
-        return_val = True
-    return return_val
- 
- 
-def get_section_text(p, text_elems):
-    """Returns the joined text of the text elements under the given paragraph tag"""
-    return_val = ''
-    text_elems = p.findall('.//w:t', ns)
-    if text_elems is not None:
-        return_val = ''.join([t.text for t in text_elems])
-    return return_val
+if __name__ == '__main__':
+    get_paragraphes('word/Test for Anki.docx')
